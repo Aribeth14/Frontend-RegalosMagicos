ff9a94759383db3a50f1730f97874702ca984118
Emily <emily.pazmino02@epn.edu.ec>
2026-05-25 20:48:29 -0500

    Cambios de diseño en Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
 import { Link } from 'react-router-dom'
+import rm from '../assets/RM.png'
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-12 py-4 bg-white border-b border-pink-100">
+    <nav className="flex items-center justify-between px-12 h-20 bg-white border-b border-pink-100">
       <div className="flex items-center gap-2">
-        <div className="w-8 h-8 border-2 border-[#00b1c1] rounded-lg flex items-center justify-center text-sm">
-          🎁
-        </div>
-        <Link to="/" className="font-bold text-[#bd3869]">Regalos Mágicos</Link>
+        <Link to="/" className="font-bold text-[#bd3869]">
+          <img src = {rm} alt="Logo" className = "h-12 w-auto object-contain"></img>
+        </Link>
       </div>
 
-      <div className="hidden md:flex gap-8 text-sm text-gray-400 font-medium">
-        <Link to="/" className="hover:text-[#bd3869] transition">Inicio</Link>
+      <div className="hidden md:flex gap-8 text-base text-gray-400 font-bold">
+        <Link to="/" className="text-[#00b1c1] transition">Inicio</Link>
         <Link to="/catalogo" className="hover:text-[#bd3869] transition">Catálogo</Link>
         <Link to="/perfil" className="hover:text-[#bd3869] transition">Mi perfil</Link>
         <Link to="/carrito" className="hover:text-[#bd3869] transition">Carrito</Link>
       </div>
 
       <Link to="/login">
-        <button className="bg-[#bd3869] text-white text-sm font-semibold px-5 py-2 rounded-full hover:opacity-90 transition">
+        <button className="bg-[#bd3869] text-white text-base font-semibold px-5 py-2 rounded-full hover:opacity-90 transition">
           Iniciar sesión
         </button>
       </Link>
